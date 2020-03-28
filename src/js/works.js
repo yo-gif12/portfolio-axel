e723701cac1ce9bb6103c65f6aad0aad85db337d
j2frise <jarce2frise@yahoo.fr>
2020-03-28 17:29:32 +0100

    correction1

--- src/js/works.js
+++ src/js/works.js
@@ -2,23 +2,25 @@
 (function() {
     if(screen.width  >= 768) {
         var page = document.getElementById('scroll__horizontal');
-        var last_pane = page.getElementsByTagName('div');
-        last_pane = last_pane[last_pane.length-1];
+        var list_pane = page.getElementsByTagName('div');
+        var last_pane = list_pane[list_pane.length-1];
+        var fisrt_pane = list_pane[0];
         var dummy_x = null;
 
         window.onscroll = function () {
-        // Horizontal Scroll.
-        var y = document.body.getBoundingClientRect().top;
-        page.scrollLeft = -y;
-        
-        // Looping Scroll.
-        var diff = window.scrollY - dummy_x;
-        if (diff > 0) {
-            window.scrollTo(0, diff);
-        }
-        else if (window.scrollY == 0) {
-            window.scrollTo(0, dummy_x);
-        }
+            // Horizontal Scroll.
+            var y = document.body.getBoundingClientRect().top;
+            page.scrollLeft = -y;
+            
+            // Looping Scroll.
+            var diff = window.scrollY - dummy_x;
+           // window.scrollTo(0, dummy_x);
+            if (diff > 0) {
+                window.scrollTo(0, dummy_x);
+            }
+            else if (window.scrollY == 0) {
+                window.scrollTo(0, diff);
+            }
         }
         // Adjust the body height if the window resizes.
         window.onresize = resize;
@@ -27,10 +29,11 @@
 
         // Reset window-based vars
         function resize() {
-        var w = page.scrollWidth-window.innerWidth+window.innerHeight;
-        document.body.style.height = w + 'px';
-        
-        dummy_x = last_pane.getBoundingClientRect().left+window.scrollY;
+            var w = page.scrollWidth-window.innerWidth+window.innerHeight;
+            document.body.style.height = w + 'px';
+            
+            //dummy_x = fisrt_pane.getBoundingClientRect().right+window.scrollY;
+            dummy_x = last_pane.getBoundingClientRect().left+window.scrollY;
         }
     }
 
